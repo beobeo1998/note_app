de7077c0f7eebece3071504c6a4cb97feddca4f6
hari <laravel1998bn@gmail.com>
2020-12-04 10:14:18 +0700

    Cập nhật note view, thêm chức năng addItem

--- lib/ui/views/note/node_view.dart
+++ lib/ui/views/note/node_view.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:note_app/ui/views/note/node_viewmodel.dart';
 import 'package:stacked/stacked.dart';
+import 'node_viewmodel.dart';
+import 'node_model.dart';
 
 class NoteView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<NoteViewModel>.reactive(
+      onModelReady: (model) => model.init(),
       builder: (context, model, child) => Scaffold(
         appBar: AppBar(title: Text(model.title)),
-        body: Center(
-          child: Text('Hello My Note'),
+          body: ListView.builder(
+          itemCount: model.items.length,
+          itemBuilder: (BuildContext context, int index) {
+            Note item = model.items[index];
+            return ListTile(
+              title: Text(item.title),
+              subtitle: Text(item.desc),
+            );
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            model.addItem();
+          },
         ),
       ),
       viewModelBuilder: () => NoteViewModel(),
